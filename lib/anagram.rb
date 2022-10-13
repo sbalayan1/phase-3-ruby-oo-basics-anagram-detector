22e18c19eb7981bdc9d9625d4851a4e9423b48b0
sbalayan1 <balayans2014@yahoo.com>
2022-10-12 23:16:54 -0700

    commit tree 26a016215ddd5b530fb20e32a770cf89c3a3bc8c

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match anagrams
+        splitWord = word.chars
+        matches = []
+        anagrams.each do |w|
+            splitCurrWord = w.chars
+            if splitWord.sort == splitCurrWord.sort
+                matches << w
+            end
+        end
+
+        return matches
+    end
+end 
+
+
+anagram = Anagram.new('gallery')
+puts anagram.match(%w[gallery regally largely])
